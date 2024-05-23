a08567c8d950134b4a5444a0cc9eb9223acdf453
orkcode <167001563+orkcode@users.noreply.github.com>
2024-05-23 21:29:40 +0300

    Update forms.py

--- qr_app/forms.py
+++ qr_app/forms.py
@@ -1,6 +1,8 @@
 import os
 from django import forms
 from qr_app.models import MediaFile
+import tempfile
+from moviepy.editor import VideoFileClip
 
 
 class MediaFileForm(forms.ModelForm):
@@ -9,6 +11,26 @@ class MediaFileForm(forms.ModelForm):
     class Meta:
         model = MediaFile
         fields = ['file', 'password']
+
+    def clean_file(self):
+        file = self.cleaned_data.get('file')
+        max_duration = 60  # Максимальная длительность видео в секундах
+
+        if file and file.content_type in ['video/mp4', 'video/quicktime', 'video/x-msvideo', 'video/x-ms-wmv']:
+            # Используем временный файл для сохранения загруженного видео
+            with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+                for chunk in file.chunks():
+                    temp_file.write(chunk)
+                temp_file.flush()  # Обеспечиваем запись всех данных на диск
+
+                # Проверьте длительность видео
+                clip = VideoFileClip(temp_file.name)
+                duration = clip.duration
+
+                if duration > max_duration:
+                    raise forms.ValidationError(f"Длительность видео не должна превышать {max_duration} секунд.")
+
+        return file
 
 
 class DeleteMediaFileForm(forms.Form):
