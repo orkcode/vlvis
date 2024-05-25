64c62b963cd54000c434cbd7472c3a9301bfbc10
orkcode <167001563+orkcode@users.noreply.github.com>
2024-05-26 01:24:51 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,8 @@ env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
